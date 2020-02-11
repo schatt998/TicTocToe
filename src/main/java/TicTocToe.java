2655f5af2a045ac349103b23fa27aa623f52e620
Swagatam Chatterjee <swagatam.chatterjee@thoughtworks.com>
2020-02-11 15:50:47 +0530

    Add | Player One will be winner if it fills a row before Player Two

--- src/main/java/TicTocToe.java
+++ src/main/java/TicTocToe.java
@@ -2,12 +2,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TicTocToe {
-    List<Integer> filledPosition = new ArrayList<>();
+    List<Integer> filledPositionByFirstPlayer = new ArrayList<>();
+    List<Integer> filledPositionBySecondPlayer = new ArrayList<>();
+    int counter;
+
+    public TicTocToe() {
+        this.counter = 0;
+        filledPositionByFirstPlayer.add(9);
+    }
 
     public void getInput(int inputPosition) throws PositionIsAlreadyFilledException {
-        filledPosition.add(2);
-        if (filledPosition.contains(inputPosition)) {
-            throw new PositionIsAlreadyFilledException();
+        if (counter % 2 == 0) {
+            if (filledPositionByFirstPlayer.contains(inputPosition))
+                throw new PositionIsAlreadyFilledException();
+            else {
+                counter += 1;
+                filledPositionByFirstPlayer.add(inputPosition);
+            }
+        } else {
+            if (filledPositionBySecondPlayer.contains(inputPosition))
+                throw new PositionIsAlreadyFilledException();
+            else {
+                counter += 1;
+                filledPositionBySecondPlayer.add(inputPosition);
+            }
         }
+    }
+
+
+    public String getWinner() {
+        if (filledPositionByFirstPlayer.contains(1) && filledPositionByFirstPlayer.contains(2) && filledPositionByFirstPlayer.contains(3))
+            return "Player One";
+        else
+            return "Player B";
     }
 }
--- src/test/java/TicTocToeTest.java
+++ src/test/java/TicTocToeTest.java
@@ -1,11 +1,36 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class TicTocToeTest {
 
     @Test
     void shouldNotifyUserThatAlreadyFilledPositionIsNotAvailableForInput() {
         TicTocToe ticTocToe = new TicTocToe();
-        Assertions.assertThrows(PositionIsAlreadyFilledException.class, () -> ticTocToe.getInput(2));
+        Assertions.assertThrows(PositionIsAlreadyFilledException.class, () -> ticTocToe.getInput(9));
+    }
+
+    @Test
+    void shouldDeclarePlayerOneAsWinnerIfItFillsARowBeforePlayerTwo() throws PositionIsAlreadyFilledException {
+        TicTocToe ticTocToe = new TicTocToe();
+        Player playerOne = mock(Player.class);
+        Player playerTwo = mock(Player.class);
+
+        when(playerOne.giveInput()).thenReturn(1, 2, 3);
+        when(playerTwo.giveInput()).thenReturn(4, 5);
+
+        ticTocToe.getInput(playerOne.giveInput());
+        ticTocToe.getInput(playerTwo.giveInput());
+        ticTocToe.getInput(playerOne.giveInput());
+        ticTocToe.getInput(playerTwo.giveInput());
+        ticTocToe.getInput(playerOne.giveInput());
+
+        String winner = ticTocToe.getWinner();
+
+        Assertions.assertEquals("Player One", winner);
+
+
     }
 }
