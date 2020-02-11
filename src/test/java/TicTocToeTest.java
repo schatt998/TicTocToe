d1a73572e8edce4dbc914557b36377061f10144d
Swagatam Chatterjee <swagatam.chatterjee@thoughtworks.com>
2020-02-11 17:01:31 +0530

    Refactor. Magic literal converted to named constants

--- src/test/java/TicTocToeTest.java
+++ src/test/java/TicTocToeTest.java
@@ -10,22 +10,26 @@ import static org.mockito.Mockito.when;
 
 class TicTocToeTest {
 
+    public static final String X = "X";
+    public static final String O = "O";
+
     @Test
     void shouldNotifyUserThatAlreadyFilledPositionIsNotAvailableForInput() {
         TicTocToe ticTocToe = new TicTocToe();
         Assertions.assertThrows(PositionIsAlreadyFilledException.class, () -> ticTocToe.getInput(9));
     }
 
-    @Test // TODO - whose player A? - PLayer cross now.
+    @Test
+        // TODO - whose player A? - PLayer cross now.
     void shouldDeclarePlayerOneAsWinnerIfItFillsARowBeforePlayerTwo2() { // TODO - inconsistency - PlayerOne vs Player A
 
         HashMap<Integer, String> board = new HashMap<>();
-        board.put(0,"X");
-        board.put(1,"X");
-        board.put(2,"X");
+        board.put(0, X);
+        board.put(1, X);
+        board.put(2, X);
 
-        board.put(4,"O");
-        board.put(7,"O");
+        board.put(4, O);
+        board.put(7, O);
 
         TicTocToe2 ticTocToe = new TicTocToe2(board); // TODO - really unhappy - specifying empty strings is really confusing - am I doing primitive obsession here?
         // TODO - I represent unmarked spaces as empty strings. Its inconvinent.
